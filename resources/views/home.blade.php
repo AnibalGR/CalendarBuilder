2b958f98143285f4f41761a76e82e1fd9d19dece
AnibalGR <32341037+AnibalGR@users.noreply.github.com>
2017-10-05 12:51:04 -0600

    Revert "Nothing"
    This reverts commit 2ec1ba10277e89e7e3782e5d64c67197853c8020, reversing
    changes made to 1978c726eac6c4cf4cba4922fe16461ebe6bd9c9.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -141,8 +141,7 @@
 		<div class='clear'></div>
 	</div></div>
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
                                     <div class='clear'></div>
                                 </div>
                             </div>
@@ -162,17 +161,12 @@
                         </div>
                     </div>
                     <div id="tabs-2">
-=======
-=======
->>>>>>> parent of 2ec1ba1... Nothing
+
                     <div class="panel-body" id="calendarPanel" style="height:auto">
                         <div class="panel-body bg-right" >
                     <div class="panel-body" id="calendarCont">
                         <div id="imagePrev" style="border: 5px"></div>
-<<<<<<< HEAD
->>>>>>> parent of 2ec1ba1... Nothing
-=======
->>>>>>> parent of 2ec1ba1... Nothing
+
                         <div id="videoDiv" style="visibility: hidden"></div>
                         <p id="topLayout" style="visibility: hidden;  width: 0px; height: 0px;">Put your image here!</p>
                         <p id="leftLayout" style="visibility: hidden;  width: 0px; height: 0px; float: left; margin-bottom: 0px;">Put your image here!</p>

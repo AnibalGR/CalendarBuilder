1424e1e9d5a460e4de16928d0fcae834db0ea60e
erickerm7 <erickerm7@hotmail.com>
2017-10-17 08:33:39 -0600

    Paginas

--- resources/views/how-it-works.blade.php
+++ resources/views/how-it-works.blade.php
@@ -1,40 +1,25 @@
 @extends('layouts.app')
 
 @section('styles')
-    <link href="{{ asset('css/slider.css') }}" rel="stylesheet" >
     <link href="{{ asset('css/custom-home.css') }}" rel="stylesheet" >
+    <link href="{{ asset('css/custom-howitworks.css') }}" rel="stylesheet" >
 @endsection
 
 @section('content')
+<section class="bgimg-1 section-title">
     <div class="container-fluid">
-        <div class="row">              
-            <div class="carousel fade-carousel slide" data-ride="carousel" data-interval="4000" id="bs-carousel">
-                    <!-- Overlay -->
-                <div class="overlay"></div>
-                    <!-- Wrapper for slides -->
-                <div class="carousel-inner">
-                    <div class="item slides active">
-                        <div class="slide-1"></div>
-                        <div class="hero">
-                            <hgroup>
-                                <h1>Appealing Digital Display</h1>        
-                                <h3>Keep patients and visitors informed and up-to-date <br>
-                                    on upcoming events and activities in your facility</h3>
-                            </hgroup>
-                            <div class="space-20">
-                            <button class="btn btn-hero btn-lg" role="button">CREATE</button>
-                            </div>
-                        </div>
-                    </div>
-                </div> 
+        <div class="row">
+            <div class="col-xs-12 text-center">
+                <div class="mt-40"></div>
+                    <h1>How it Works</h1>
             </div>
         </div>
     </div>
-
+</section>
         <div class="container">
             <div class="row space-40">
                 <div class="col-lg-12 txtcenter color-1">
-                <h1>How It Works</h1>
+                    <!--<h1>How It Works</h1>-->
                 </div>
             </div>
             <div class="row space-20">
@@ -56,45 +41,72 @@
             </div>
         </div>
 
-        <div class="container full-width bg-gray">
+        <div class="container full-width bg-div-3">
             <div class="row space-60">
-                <div class="col-sm-4">
-                    <div class="testimonial">
-                        <span class="fa fa-quote-left fa-icons" aria-hidden="true"></span>
-                    </div>                    
-                </div>
-                <div class="col-sm-6 testimonial-2 space-40">
-                <p>We are so exited to have signed up for this service. Digital Activity Clendars has helped our business
-                save time and money, and our facility looks better than over. This service has everything I need.<p>
-                </div>
-                <div class="col-sm-2"></div>
+                <div class="col-xs-12">
+                    <div class="button-sign-up">
+                    <a href="#">SIGN UP</a>
+                    </div>
             </div>
         </div>
-        
-        <div class="container space-40">
-            <div class="row">
-                <div class="col-lg-12 txtcenter color-2">
-                <h1>Customized for any display.</h1>
-                </div>
-            </div>
-            <div class="row space-20">
-                <div class="col-sm-4 hIcons-2 space-40">
-                    <img src="{{ asset('img/home/home-4.png') }}" class="img-responsive">
-                    <div class="full-width space-40"></div>
-                    <h3>Computer Screens</h3>
-                </div>
-                <div class="col-sm-4 hIcons-2 space-40">
-                    <img src="{{ asset('img/home/home-5.png') }}" class="img-responsive">
-                    <div class="full-width space-40"></div>
-                    <h3>Flatscreen TV's</h3>
-                </div>
-                <div class="col-sm-4 hIcons-2 space-40">
-                    <img src="{{ asset('img/home/home-6.png') }}" class="img-responsive">
-                    <div class="full-width space-40"></div>
-                    <h3>Patient rooms</h3>
-                </div>
+        </div>
+<section>
+<div class="container-fluid bg-div-2">
+    <div class="mt-50"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h2 class="color-2">Creating your custom calendar</h2>
             </div>
         </div>
+        <div class="row">
+            <div class="col-md-12">
+                <h4 class="color-1">Lorem Ipsum</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id ipsum diam. Nulla facilisi. 
+                    Sed suscipit sem a mauris commodo aliquet. In sit amet nulla vel nisi congue pulvinar nec et dui nulla facilisi. 
+                    Sed vestibulum sem vel elit tincidunt ultricies. Nullam rutrum lacus et elit cursus, at dapibus ante ultrices.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="hIcons-2">
+                    <ul>
+                                <li><p><strong><i class="fa fa-cloud-upload list-green" aria-hidden="true"></i></strong></p></li>
+                    </ul>
+                </div>
+                <h4 class="color-2 text-center">Step 1</h4>
+                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id ipsum diam. Nulla facilisi. 
+                    Sed suscipit sem a mauris commodo aliquet. In sit amet nulla vel nisi congue pulvinar nec et dui nulla facilisi. 
+                    Sed vestibulum sem vel elit tincidunt ultricies. Nullam rutrum lacus et elit cursus, at dapibus ante ultrices.</p>
+            </div>
+            <div class="col-md-4">
+                <div class="hIcons-2">
+                    <ul>
+                                <li><p><strong><i class="fa fa-cogs list-green" aria-hidden="true"></i></strong></p></li>
+                    </ul>
+                </div>
+                <h4 class="color-2 text-center">Step 1</h4>
+                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id ipsum diam. Nulla facilisi. 
+                    Sed suscipit sem a mauris commodo aliquet. In sit amet nulla vel nisi congue pulvinar nec et dui nulla facilisi. 
+                    Sed vestibulum sem vel elit tincidunt ultricies. Nullam rutrum lacus et elit cursus, at dapibus ante ultrices.</p>
+            </div>
+            <div class="col-md-4">
+                <div class="hIcons-2">
+                    <ul>
+                                <li><p><strong><i class="fa fa-tv list-green" aria-hidden="true"></i></strong></p></li>
+                    </ul>
+                </div>
+                <h4 class="color-2 text-center">Step 1</h4>
+                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id ipsum diam. Nulla facilisi. 
+                    Sed suscipit sem a mauris commodo aliquet. In sit amet nulla vel nisi congue pulvinar nec et dui nulla facilisi. 
+                    Sed vestibulum sem vel elit tincidunt ultricies. Nullam rutrum lacus et elit cursus, at dapibus ante ultrices.</p>
+            </div>
+        </div>
+    </div>
+    <div class="mt-80"></div>
+</div>
+</section>
+
     
 <div class="container-fluid bottomSec-1">
     <div class="col-xs-12 space-60">
--- resources/views/plans/index.blade.php
+++ resources/views/plans/index.blade.php
@@ -6,195 +6,186 @@
 
 
 @section('content')
-
-<section class="bgimg-2">
+<section class="bgimg-1 section-title">
     <div class="container-fluid">
-        
         <div class="row">
-            <div class="col-xs-12">
-                <div class="mt-80">
-                    Pricin Page
-                </div>
+            <div class="col-xs-12 text-center">
+                <div class="mt-40"></div>
+                    <h1>Pricing Title Here</h1>
             </div>
         </div>
     </div>
 </section>
-
-
-
-
-<!-- Black Pricing Table -->
-	<section class="section-padding black-table-area bgimg-1 bg-additional">
-		<div class="container">
-			<div class="row">
-				<!-- Heading -->
-				<div class="col-md-12 text-center">
-					<h1 class="section-title purple-heading">Pricing Title here</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod<br/> tempor incididunt ut labore et dolore magna aliqua.</p>
-				</div>
-				<!-- Pricing Table Area -->
-				<div class="gg-pricing-table dark-table col-md-12 mt-50">
-					<!-- Single Table -->
-					<div class="col-md-4">
-						<div class="single-pricing-table text-center clearfix">
-							<!-- Heading -->
-							<div class="pricing-table-heading">
-								<h2>Free</h2>
-								<p>Edit and view only</p>
-							</div>
-							<!-- Price -->
-							<div class="price">
-								<span>FREE</span>
-							</div>
-							<!-- Price Item -->
-							<div class="pricing-item">
-                                                            <div class="row">
-                                                            <div class="col-xs-6" style="text-align: right; font-size: 22px;">
-                                                                <ul>
-									<li><p><strong><i class="fa fa-check-circle" aria-hidden="true"></i></strong></p></li>
-									<li><p><strong><i class="fa fa-check-circle" aria-hidden="true"></i></strong></p></li>
-									<li><p><strong><i class="fa fa-times-circle" aria-hidden="true"></i></strong></p></li>
-									<li><p><strong><i class="fa fa-times-circle" aria-hidden="true"></i></strong></p></li>
-								</ul>
-                                                            </div>
-                                                                <div class="col-xs-6" style="text-align: left; margin-left: 0px !important; padding-left: 0px;">
-                                                                <ul>
-									<li><p>View</p></li>
-									<li><p>Editor</p></li>
-									<li><p>Save</p></li>
-									<li><p>Export</p></li>
-								</ul>
-                                                            </div>
-                                                            </div>
-                                                        </div>
-							<!-- Button -->
-							<div class="pricing-button">
-								<a href="#" class="btn btn-pricing">Sign Up</a>
-							</div>
-						</div>
-					</div>
-					<!-- Single Table -->
-					<div class="col-md-4">
-						<div class="single-pricing-table pricing-special text-center clearfix">
-							<!-- Heading -->
-							<div class="pricing-table-heading">
-								<h2>Monthly Plan</h2>
-								<p>Fully dadicated to the small project</p>
-							</div>
-							<!-- Price -->
-							<div class="price">
-								<span>$39.99</span>
-							</div>
-							<!-- Price Item -->
-							<div class="pricing-item">
-								<div class="row">
-                                                            <div class="col-xs-6" style="text-align: right; font-size: 22px;">
-                                                                <ul>
-									<li><p><strong><i class="fa fa-check-circle" aria-hidden="true"></i></strong></p></li>
-									<li><p><strong><i class="fa fa-check-circle" aria-hidden="true"></i></strong></p></li>
-									<li><p><strong><i class="fa fa-check-circle" aria-hidden="true"></i></strong></p></li>
-									<li><p><strong><i class="fa fa-check-circle" aria-hidden="true"></i></strong></p></li>
-								</ul>
-                                                            </div>
-                                                                <div class="col-xs-6" style="text-align: left; margin-left: 0px !important; padding-left: 0px;">
-                                                                <ul>
-									<li><p>View</p></li>
-									<li><p>Editor</p></li>
-									<li><p>Save</p></li>
-									<li><p>Export</p></li>
-								</ul>
-                                                            </div>
-                                                            </div>
-							</div>
-							<!-- Button -->
-							<div class="pricing-button">
-								<a href="#" class="btn btn-pricing">Sign Up</a>
-							</div>
-						</div>
-					</div>
-					<!-- Single Table -->
-					<div class="col-md-4">
-						<div class="single-pricing-table text-center clearfix">
-							<!-- Heading -->
-							<div class="pricing-table-heading">
-								<h2>Yearly Plan</h2>
-								<p>Fully dadicated to the large project</p>
-							</div>
-							<!-- Price -->
-							<div class="price">
-								<span>$400</span>
-							</div>
-							<!-- Price Item -->
-							<div class="pricing-item">
-								<div class="row">
-                                                            <div class="col-xs-6" style="text-align: right; font-size: 22px;">
-                                                                <ul>
-									<li><p><strong><i class="fa fa-check-circle" aria-hidden="true"></i></strong></p></li>
-									<li><p><strong><i class="fa fa-check-circle" aria-hidden="true"></i></strong></p></li>
-									<li><p><strong><i class="fa fa-check-circle" aria-hidden="true"></i></strong></p></li>
-									<li><p><strong><i class="fa fa-check-circle" aria-hidden="true"></i></strong></p></li>
-								</ul>
-                                                            </div>
-                                                                <div class="col-xs-6" style="text-align: left; margin-left: 0px !important; padding-left: 0px;">
-                                                                <ul>
-									<li><p>View</p></li>
-									<li><p>Editor</p></li>
-									<li><p>Save</p></li>
-									<li><p>Export</p></li>
-								</ul>
-                                                            </div>
-                                                            </div>
-							</div>
-							<!-- Button -->
-							<div class="pricing-button">
-								<a href="#" class="btn btn-pricing">Sign Up</a>
-							</div>
-						</div>
-					</div>
-					<!-- Credits -->
-					<div class="design-credit text-center col-md-12 mt-40">
-						<p>Have questions, we'll be happy to answer <a href="#" target="_blank">here.</a></p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-
-
-
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Choose your plan</div>
-
-                <div class="panel-body">
-                    <ul class="list-group">
-                        @foreach ($plans as $plan)
-                            <li class="list-group-item clearfix">
-                                <div class="pull-left">
-                                    <h4>{{ $plan->name }}</h4>
-                                    <h4>${{ number_format($plan->cost, 2) }}</h4>
-                                    @if ($plan->description)
-                                        <p>{{ $plan->description }}</p>
-                                    @endif
-                                </div>
-                                @if(Auth::user())
-                                    @if (!Auth::user()->subscribedToPlan($plan->braintree_plan, 'main'))
-                                        <a href="{{ url('/plan', $plan->slug) }}" class="btn btn-default pull-right">Choose Plan</a>
-                                    @else
-                                        <a href="" class="btn btn-default pull-right">You are already subscribed to this plan</a>
-                                    @endif
-                                @endif
-
-                            </li>
-                        @endforeach
-                    </ul>
+<section>
+    <div class="container section-table-pricing">
+        <div class="col-md-4 mt-40">
+            <div class="table-pricing">
+                <div class="pricing-group">
+                    <h2>FREE</h2>
+                    <p>Edit and view only</p>
                 </div>
+                <div class="princint-1">
+                    <span>FREE</span>
+                </div>
+                <!-- Price Item -->
+                <div class="pricing-item">
+                    <div class="row">
+                        <div class="col-xs-6" style="text-align: right; font-size: 22px;">
+                            <ul>
+                                <li><p><strong><i class="fa fa-check-circle list-green" aria-hidden="true"></i></strong></p></li>
+                                <li><p><strong><i class="fa fa-check-circle list-green" aria-hidden="true"></i></strong></p></li>
+                                <li><p><strong><i class="fa fa-times-circle list-green" aria-hidden="true"></i></strong></p></li>
+                                <li><p><strong><i class="fa fa-times-circle list-green" aria-hidden="true"></i></strong></p></li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-6" style="text-align: left; margin-left: 0px !important; padding-left: 0px;">
+                            <ul>
+                                <li><p>View</p></li>
+                                <li><p>Editor</p></li>
+                                <li><p>Save</p></li>
+                                <li><p>Export</p></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="button-sign-up">
+                    <a href="#">SIGN UP</a>
+                </div>
+                <div class="pricing-table-bottom"></div>
+            </div>
+        </div>
+        <div class="col-md-4 mt-40">
+            <div class="table-pricing">
+                <div class="pricing-group">
+                    <h2>Monthly Plan</h2>
+                    <p>Edit and view, save and export</p>
+                </div>
+                <div class="princint-1">
+                    <span>Monthly</span>
+                </div>
+                <!-- Price Item -->
+                <div class="pricing-item">
+                    <div class="row">
+                        <div class="col-xs-6" style="text-align: right; font-size: 22px;">
+                            <ul>
+                                <li><p><strong><i class="fa fa-check-circle list-green" aria-hidden="true"></i></strong></p></li>
+                                <li><p><strong><i class="fa fa-check-circle list-green" aria-hidden="true"></i></strong></p></li>
+                                <li><p><strong><i class="fa fa-check-circle list-green" aria-hidden="true"></i></strong></p></li>
+                                <li><p><strong><i class="fa fa-check-circle list-green" aria-hidden="true"></i></strong></p></li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-6" style="text-align: left; margin-left: 0px !important; padding-left: 0px;">
+                            <ul>
+                                <li><p>View</p></li>
+                                <li><p>Editor</p></li>
+                                <li><p>Save</p></li>
+                                <li><p>Export</p></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="button-sign-up">
+                    <a href="#">SIGN UP</a>
+                </div>
+                <div class="pricing-table-bottom"></div>
+            </div>
+        </div>
+        <div class="col-md-4 mt-40">
+            <div class="table-pricing">
+                <div class="pricing-group">
+                    <h2>Yearly Plan</h2>
+                    <p>Edit and view, save and export</p>
+                </div>
+                <div class="princint-1">
+                    <span>Yearly</span>
+                </div>
+                <!-- Price Item -->
+                <div class="pricing-item">
+                    <div class="row">
+                        <div class="col-xs-6" style="text-align: right; font-size: 22px;">
+                            <ul>
+                                <li><p><strong><i class="fa fa-check-circle list-green" aria-hidden="true"></i></strong></p></li>
+                                <li><p><strong><i class="fa fa-check-circle list-green" aria-hidden="true"></i></strong></p></li>
+                                <li><p><strong><i class="fa fa-check-circle list-green" aria-hidden="true"></i></strong></p></li>
+                                <li><p><strong><i class="fa fa-check-circle list-green" aria-hidden="true"></i></strong></p></li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-6" style="text-align: left; margin-left: 0px !important; padding-left: 0px;">
+                            <ul>
+                                <li><p>View</p></li>
+                                <li><p>Editor</p></li>
+                                <li><p>Save</p></li>
+                                <li><p>Export</p></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="button-sign-up">
+                    <a href="#">SIGN UP</a>
+                </div>
+                <div class="pricing-table-bottom"></div>
             </div>
         </div>
     </div>
+</section>
+<section>
+    <div class="container-fluid">
+        <div class="mt-100"></div>
+    </div>
+</section>
+<section>
+<div class="container-fluid bg-div-2">
+    <div class="mt-50"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h2 class="color-2">Frequently Asked Questions</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <h4 class="color-1">Lorem Ipsum</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id ipsum diam. Nulla facilisi. 
+                    Sed suscipit sem a mauris commodo aliquet. In sit amet nulla vel nisi congue pulvinar nec et dui nulla facilisi. 
+                    Sed vestibulum sem vel elit tincidunt ultricies. Nullam rutrum lacus et elit cursus, at dapibus ante ultrices.</p>
+            </div>
+            <div class="col-md-4">
+                <h4 class="color-1">Lorem Ipsum</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id ipsum diam. Nulla facilisi. 
+                    Sed suscipit sem a mauris commodo aliquet. In sit amet nulla vel nisi congue pulvinar nec et dui nulla facilisi. 
+                    Sed vestibulum sem vel elit tincidunt ultricies. Nullam rutrum lacus et elit cursus, at dapibus ante ultrices.</p>
+            </div>
+            <div class="col-md-4">
+                <h4 class="color-1">Lorem Ipsum</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id ipsum diam. Nulla facilisi. 
+                    Sed suscipit sem a mauris commodo aliquet. In sit amet nulla vel nisi congue pulvinar nec et dui nulla facilisi. 
+                    Sed vestibulum sem vel elit tincidunt ultricies. Nullam rutrum lacus et elit cursus, at dapibus ante ultrices.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <h4 class="color-1">Lorem Ipsum</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id ipsum diam. Nulla facilisi. 
+                    Sed suscipit sem a mauris commodo aliquet. In sit amet nulla vel nisi congue pulvinar nec et dui nulla facilisi. 
+                    Sed vestibulum sem vel elit tincidunt ultricies. Nullam rutrum lacus et elit cursus, at dapibus ante ultrices.</p>
+            </div>
+            <div class="col-md-4">
+                <h4 class="color-1">Lorem Ipsum</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id ipsum diam. Nulla facilisi. 
+                    Sed suscipit sem a mauris commodo aliquet. In sit amet nulla vel nisi congue pulvinar nec et dui nulla facilisi. 
+                    Sed vestibulum sem vel elit tincidunt ultricies. Nullam rutrum lacus et elit cursus, at dapibus ante ultrices.</p>
+            </div>
+            <div class="col-md-4">
+                <h4 class="color-1">Lorem Ipsum</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id ipsum diam. Nulla facilisi. 
+                    Sed suscipit sem a mauris commodo aliquet. In sit amet nulla vel nisi congue pulvinar nec et dui nulla facilisi. 
+                    Sed vestibulum sem vel elit tincidunt ultricies. Nullam rutrum lacus et elit cursus, at dapibus ante ultrices.</p>
+            </div>
+        </div>
+    </div>
+    <div class="mt-80"></div>
 </div>
+</section>
 @endsection
 
 <!-- Scripts -->

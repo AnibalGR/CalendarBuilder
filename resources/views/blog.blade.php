00fbb3fc316877721b8fd69c9bbacbb02edb80e6
erickerm7 <erickerm7@hotmail.com>
2017-11-07 17:47:08 -0600

    Blog height

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,8 +4,8 @@
 <div class="container-fluid">
   <div class="row">
       <div class="col-md-12">
-<div style="width:100%; height:100%;">
-<iframe src="{{ asset('/blog-content') }}" width="100%" height="auto" frameborder="0" marginwidth="0" marginheight="0">Test of Blog</iframe>
+<div style="width:100%; height:auto;">
+<iframe src="{{ asset('/blog-content') }}" width="100%" height="1000px" frameborder="0" marginwidth="0" marginheight="0">Test of Blog</iframe>
 </div>
 </div>
 </div>

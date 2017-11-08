c2ec959a313e6d22b39680c6fb49c0567545967d
AnibalGR <32341037+AnibalGR@users.noreply.github.com>
2017-11-07 22:52:48 -0600

    iFrame fixed

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
+@section('styles')
+<style>iframe{width: 1px;min-width: 100%;}</style>
+@endsection
+
 @section('content')
-<div class="container-fluid">
-  <div class="row">
-      <div class="col-md-12">
-<div style="width:100%; height:auto;">
-<iframe src="{{ asset('/blog-content') }}" width="100%" height="1000px" frameborder="0" marginwidth="0" marginheight="0">Test of Blog</iframe>
-</div>
-</div>
-</div>
-</div>
+                <iframe id="myIframe" src="{{ asset('/blog-content') }}" scrolling="no" onload="this.style.height=this.contentDocument.body.scrollHeight +'px';"></iframe>
+@endsection
+
+@section('scripts')
+
 @endsection

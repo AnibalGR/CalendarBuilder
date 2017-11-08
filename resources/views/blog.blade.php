1bb0e7f9fb41cd9e9c869b52926bebf0c8999a96
AnibalGR <32341037+AnibalGR@users.noreply.github.com>
2017-11-07 23:32:35 -0600

    Fixed iFrame

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -5,5 +5,19 @@
 @endsection
 
 @section('content')
-                <iframe id="myIframe" src="{{ asset('/blog-content') }}" scrolling="no" onload="this.style.height=this.contentDocument.body.scrollHeight +'px';"></iframe>
+                <iframe id="myIframe" src="{{ asset('/blog-content') }}" scrolling="no" onload="this.style.height=this.contentDocument.body.scrollHeight + 'px';"></iframe>
+@endsection
+
+@section('scripts')
+<script>
+$(function(){
+    // Resize canvas
+    window.addEventListener('resize', resizeIframe, false);
+    
+    function resizeIframe(){
+        document.getElementById("myIframe").style.height = document.getElementById("myIframe").contentDocument.body.scrollHeight + 'px'
+    }
+    
+})
+</script>
 @endsection

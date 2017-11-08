d095869150824fa8c88f1d5bf975671b6bb1f1dd
AnibalGR <32341037+AnibalGR@users.noreply.github.com>
2017-11-07 22:54:13 -0600

    Ahora si

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -7,16 +7,3 @@
 @section('content')
                 <iframe id="myIframe" src="{{ asset('/blog-content') }}" scrolling="no" onload="this.style.height=this.contentDocument.body.scrollHeight +'px';"></iframe>
 @endsection
-
-@section('scripts')
-
-<div class="container-fluid">
-  <div class="row">
-      <div class="col-md-12">
-<div style="width:100%; height:1000px">
-<iframe src="{{ asset('/blog-content') }}" width="100%" height="100%" frameborder="0" marginwidth="0" marginheight="0">Test of Blog</iframe>
-</div>
-</div>
-</div>
-</div>
-@endsection

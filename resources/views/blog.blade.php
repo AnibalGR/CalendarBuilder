d647141af6678aaedba759eb25d14c333f3512ca
erickerm7 <erickerm7@hotmail.com>
2017-11-07 18:00:00 -0600

    cambio

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,8 +4,8 @@
 <div class="container-fluid">
   <div class="row">
       <div class="col-md-12">
-<div style="width:100%; height:auto;">
-<iframe src="{{ asset('/blog-content') }}" width="100%" height="1000px" frameborder="0" marginwidth="0" marginheight="0">Test of Blog</iframe>
+<div style="width:100%; height:1000px">
+<iframe src="{{ asset('/blog-content') }}" width="100%" height="100%" frameborder="0" marginwidth="0" marginheight="0">Test of Blog</iframe>
 </div>
 </div>
 </div>
